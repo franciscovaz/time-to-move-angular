2f5f73303d22264a56b464983958686668a4a39a
Francisco Vaz <ftomasvaz@gmail.com>
2021-03-02 21:29:04 +0000

    create experience bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExperienceBarComponent } from './components/experience-bar/experience-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperienceBarComponent
   ],
   imports: [
     BrowserModule,
