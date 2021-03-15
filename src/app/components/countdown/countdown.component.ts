859ceab815c8706203cd0ffe96e0b65d04a36468
Francisco Vaz <ftomasvaz@gmail.com>
2021-03-15 22:29:46 +0000

    add logic to countdown

--- src/app/components/countdown/countdown.component.ts
+++ src/app/components/countdown/countdown.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountdownComponent implements OnInit {
 
+  time = 25 * 60;
+  isActive = false;
+  hasFinished = false;
+
+  minutes = Math.floor(this.time / 60);
+  seconds = this.time % 60;
+
+  countdownTimeout: NodeJS.Timeout;
+
+  minuteLeft = String(this.minutes).padStart(2, '0').split('')[0];
+  minuteRight = String(this.minutes).padStart(2, '0').split('')[1];
+
+  secondRight = String(this.seconds).padStart(2, '0').split('')[1];
+  secondLeft = String(this.seconds).padStart(2, '0').split('')[1];
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+    if (this.isActive && this.time > 0) {
+      this.countdownTimeout = setTimeout(() => {
+        this.time = this.time - 1;
+      }, 1000);
+    } else if (this.isActive && this.time === 0) {
+      this.hasFinished = true;
+      this.isActive = false;
+      // TODO começar novo desafio
+    }
+  }
+
+  startCountdown() {
+    this.isActive = true;
+
+  }
+
+  resetCountdown() {
+    clearTimeout(this.countdownTimeout);
+    this.isActive = false;
+    this.time = 25 * 60;
+    this.hasFinished = false;
   }
 
 }
