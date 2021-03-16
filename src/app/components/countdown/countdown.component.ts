943c2e23635816f443b2d473fa07fdf23bbd7544
Francisco Vaz <ftomasvaz@gmail.com>
2021-03-16 21:35:24 +0000

    countdown logic

--- src/app/components/countdown/countdown.component.ts
+++ src/app/components/countdown/countdown.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { interval, Observable, Subscription, timer } from 'rxjs';
+
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-countdown',
@@ -7,47 +10,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountdownComponent implements OnInit {
 
-  time = 25 * 60;
+  time = 0.1 * 60;
   isActive = false;
   hasFinished = false;
 
   minutes = Math.floor(this.time / 60);
   seconds = this.time % 60;
 
-  countdownTimeout;
-
   minuteLeft = String(this.minutes).padStart(2, '0').split('')[0];
   minuteRight = String(this.minutes).padStart(2, '0').split('')[1];
 
+  secondLeft = String(this.seconds).padStart(2, '0').split('')[0];
   secondRight = String(this.seconds).padStart(2, '0').split('')[1];
-  secondLeft = String(this.seconds).padStart(2, '0').split('')[1];
 
 
+
+
+
+  countdownTimeout$;
+
+  counter$: Observable<number>;
+  subscription: Subscription;
+
+  private firstSub: Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(String(this.seconds).padStart(2, '0').split('')[0]);
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+
+  countdown() {
+
+
+
     if (this.isActive && this.time > 0) {
-      this.countdownTimeout = setTimeout(() => {
+
+      this.firstSub = interval(1000).subscribe(() => {
         this.time = this.time - 1;
-      }, 1000);
+
+        if (this.isActive && this.time === 0) {
+          this.hasFinished = true;
+          this.isActive = false;
+          // TODO começar novo desafio
+
+
+          this.firstSub.unsubscribe();
+        }
+
+
+
+        this.minutes = Math.floor(this.time / 60);
+        this.seconds = this.time % 60;
+
+
+        this.minuteLeft = String(this.minutes).padStart(2, '0').split('')[0];
+        this.minuteRight = String(this.minutes).padStart(2, '0').split('')[1];
+
+        this.secondLeft = String(this.seconds).padStart(2, '0').split('')[0];
+        this.secondRight = String(this.seconds).padStart(2, '0').split('')[1];
+
+      })
+
+
+
+
     } else if (this.isActive && this.time === 0) {
       this.hasFinished = true;
       this.isActive = false;
       // TODO começar novo desafio
+      console.log('cheguei');
+
+
+      this.firstSub.unsubscribe();
     }
+
   }
 
   startCountdown() {
     this.isActive = true;
+    this.countdown();
 
   }
 
   resetCountdown() {
-    clearTimeout(this.countdownTimeout);
+    clearTimeout(this.countdownTimeout$);
     this.isActive = false;
     this.time = 25 * 60;
     this.hasFinished = false;
   }
 
 }
+
+
