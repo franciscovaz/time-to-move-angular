f13c718d56e029ab3244c6297a096e6128f5dbe7
Francisco Vaz <ftomasvaz@gmail.com>
2021-04-05 23:02:54 +0100

    remove console logs

--- src/app/store/challenge/challenge.effects.ts
+++ src/app/store/challenge/challenge.effects.ts
@@ -1,48 +1,26 @@
 import { Injectable } from "@angular/core";
 import { createEffect, Actions, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
-import { map, switchMap, tap, withLatestFrom } from 'rxjs/operators';
+import { tap, withLatestFrom } from 'rxjs/operators';
 
 import * as fromAppRoot from '../app.reducer';
 import * as ChallengeActions from '../challenge/challenge.actions';
 
 @Injectable()
 export class ChallengeEffects {
-  /* storeChallenge = createEffect(() =>
-    this.actions$.pipe(
-      ofType(ChallengeActions.storeChallenge),
-      withLatestFrom(this.store.select('challenge')),
-      switchMap(([actionData, challengeState]) => {
-
-
-      }),
-      map(challenge => {
-        console.log('challenge: ', challenge);
-        return true;
-      })
-    ), { dispatch: false }
-  ) */
   completeChallenge = createEffect(() =>
     this.actions$.pipe(
       ofType(ChallengeActions.isChallengeSucceeded),
       withLatestFrom(this.store.select('challenge')),
       tap(([action, store]) => {
         // se action.isChallengSuccess dispara uma action para atualizar a store...
-        console.log('action Effect: ', action);
-        console.log('store Effect: ', store);
-
         const { amount } = action;
         const { experienceToNextLevel, currentExperience } = store.challenge;
 
         let finalExperience = currentExperience + amount;
 
-        console.log('Current Experience: ', currentExperience);
-        console.log('To next level: ', experienceToNextLevel);
-        console.log('Final Experience: ', finalExperience);
-
         if (finalExperience >= experienceToNextLevel) {
           finalExperience = finalExperience - experienceToNextLevel;
-          console.log('Final Experience: ', finalExperience);
           this.store.dispatch(ChallengeActions.levelUp());
         }
         this.store.dispatch(ChallengeActions.setCurrentExperience({ currentExperience: finalExperience }))
