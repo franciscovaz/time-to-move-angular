4f1aa7be7046d4ef20750871b784eb9828ee642b
Francisco Vaz <ftomasvaz@gmail.com>
2021-04-08 21:53:22 +0100

    change default profile image

--- src/app/store/profile/profile.reducer.ts
+++ src/app/store/profile/profile.reducer.ts
@@ -10,7 +10,7 @@ export interface State {
 const initialState: State = {
   profile: {
     name: 'John Doe',
-    imgUrl: 'https://github.com/franciscovaz.png',
+    imgUrl: 'https://icon2.cleanpng.com/20180405/rge/kisspng-login-google-account-computer-icons-user-activity-5ac6bbe6ad7041.1621164815229736707104.jpg',
     isProfileChangeModalOpen: false
   }
 }
