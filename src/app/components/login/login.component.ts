2c754f526321cc3d26e26c4474c486e3f29896dc
Francisco Vaz <ftomasvaz@gmail.com>
2021-04-18 09:51:56 +0100

    login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface User {
+  email: string;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  user: User;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.user = {
+      email: ''
+    }
+  }
+
+  handleSubmit(): void {
+    console.log('email: ', this.user.email);
   }
 
 }
