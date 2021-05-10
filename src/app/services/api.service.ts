e709ed050f502650d2f78bfb2499633faf71f1eb
Francisco Vaz <ftomasvaz@gmail.com>
2021-05-10 22:33:12 +0100

    create api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    private readonly http: HttpClient
+  ) { }
+
+  getUsers() {
+    return this.http.get('https://time-to-move-14d11-default-rtdb.firebaseio.com/users.json').pipe(
+      map(respData => {
+        const usersArray = [];
+        for (const key in respData) {
+          if (respData.hasOwnProperty(key)) {
+            usersArray.push({ ...respData[key], id: key })
+          }
+        }
+        return usersArray;
+      }))
+  }
+}
