44e5586de1443603f4065219c8c3f30fae1a98b3
Francisco Vaz <ftomasvaz@gmail.com>
2021-05-10 22:33:22 +0100

    get users from api service

--- src/app/components/after-login-to-fix/after-login-to-fix.component.ts
+++ src/app/components/after-login-to-fix/after-login-to-fix.component.ts
@@ -5,6 +5,7 @@ import * as CountdownActions from '../../store/countdown/countdown.actions';
 import { HttpClient } from '@angular/common/http';
 import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
+import { ApiService } from 'src/app/services/api.service';
 
 
 interface User {
@@ -30,6 +31,7 @@ export class AfterLoginToFixComponent implements OnInit {
   constructor(
     private http: HttpClient,
     private store: Store<fromAppRoot.AppState>,
+    private readonly apiService: ApiService
   ) { }
 
 
@@ -38,23 +40,14 @@ export class AfterLoginToFixComponent implements OnInit {
     localStorage.setItem('menuToShow', 'Ranking');
 
     // TODO mudar esta approach
-    this.http.get('https://time-to-move-14d11-default-rtdb.firebaseio.com/users.json').pipe(
-      map(respData => {
-        const usersArray = [];
-        for (const key in respData) {
-          if (respData.hasOwnProperty(key)) {
-            usersArray.push({ ...respData[key], id: key })
-          }
-        }
-        return usersArray;
-      })).subscribe(users => {
-        // ja existe este email, vamos redirecionar e atualizar a store com a info do user
-        for (var i = 0; i < users.length; i++) {
+    this.apiService.getUsers().subscribe(users => {
+      // ja existe este email, vamos redirecionar e atualizar a store com a info do user
+      for (var i = 0; i < users.length; i++) {
 
-          if (users[i].email === localStorage.getItem('email')) {
-            this.store.dispatch(CountdownActions.updateSumCountdownTime({ actualCountdownTime: users[i].sumCountdownTime }));
-          }
+        if (users[i].email === localStorage.getItem('email')) {
+          this.store.dispatch(CountdownActions.updateSumCountdownTime({ actualCountdownTime: users[i].sumCountdownTime }));
         }
-      });
+      }
+    });
   }
 }
